fde39da9603548441474d7e17cb2bf346dd1a788
king1235678910111213 <100225626+king1235678910111213@users.noreply.github.com>
2022-02-22 12:57:20 -0500

    Add files via upload
    failed command line version

---
+++ quantumsecuritesdimension/Assgaurdya/src/com/quantum/assgaurdya/Assgaurdya.java
@@ -0,0 +1,13 @@
+package com.quantum.assgaurdya;
+
+public class Assgaurdya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Head Spinner !!");
+		System.out.println("If Accident Nature God Godess Element Create Universe !!");
+		
+	}
+
+}
